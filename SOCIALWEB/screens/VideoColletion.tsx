a1366e06c96f3cda5e38fcadb8a7a8829f31727c
Swarnendu Chatterjee <swarnendubabai32@gmail.com>
2022-01-29 22:45:09 +0530

    Icons addded

--- SOCIALWEB/screens/VideoColletion.tsx
+++ SOCIALWEB/screens/VideoColletion.tsx
@@ -1,6 +1,13 @@
 import React from "react";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { TabBar } from "react-native-tab-view";
+import Icon, {
+    AroundTheClockBold,
+    ArrowDown,
+    BookmarkOutlineBold,
+    BookmarkOutlineRegular,
+    BookmarkSolid,
+} from "../components/Icons";
 import { globalColors, globalLayouts } from "../utility/styles";
 import { BoldText } from "../utility/ui";
 
@@ -10,7 +17,13 @@ const VideoCollection = () => {
             edges={["left", "right"]}
             style={[globalLayouts.tabLayout, globalColors.screenColor]}
         >
-            <BoldText>video collection</BoldText>
+            <Icon name="BookmarkOutlineBold" size={32} color="red" />
+            <Icon name="AroundTheClockRegular" size={32} color="red" />
+            <Icon name="ArrowDown" size={32} color="red" />
+            <Icon name="BookmarkOutlineRegular" size={32} color="red" />
+            <Icon name="EditSolid" size={32} color="red" />
+            <Icon name="HistoryRegular" size={32} color="red" />
+            <Icon name="TrendingSolid" size={32} color="red" />
         </SafeAreaView>
     );
 };
